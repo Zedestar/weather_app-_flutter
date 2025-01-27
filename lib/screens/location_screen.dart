beaf0c8c5121ca4437d2da324f73a2171d12fa0c
Melkizedeki <mmagota2@gmail.com>
2025-01-27 20:09:19 +0300

    commit 2

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather.dart';
 import 'dart:convert';
 
 class LocationScreen extends StatefulWidget {
@@ -11,18 +12,24 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  late dynamic temperature;
-  late dynamic condition;
-  late dynamic cityName;
+  late double temperature;
+  late int condition;
+  late String cityName;
 
   void updateUI(dynamic weatherData) {
     dynamic data = jsonDecode(weatherData);
     temperature = data["main"]["temp"];
-    condition = data["weather"][0]["main"];
+    condition = data["weather"][0]["id"];
     cityName = data["name"];
+    // Creating the Weather Model
+    WeatherModel weatherModer = WeatherModel();
+    String weatherIcon = weatherModer.getWeatherIcon(condition);
+    String weatherMessage = weatherModer.getMessage(temperature);
     print(temperature);
     print(condition);
     print(cityName);
+    print(weatherIcon);
+    print(weatherMessage);
   }
 
   @override
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,1 +1,33 @@
-import 'package:flutter/material.dart';
+class WeatherModel {
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return "⛈️"; // Thunderstorm
+    } else if (condition < 400) {
+      return "🌧️"; // Drizzle
+    } else if (condition < 600) {
+      return "☔"; // Rain
+    } else if (condition < 700) {
+      return "❄"; // Snow
+    } else if (condition < 800) {
+      return "☁️"; // Clouds
+    } else if (condition == 800) {
+      return "☀️"; // Clear sky
+    } else if (condition <= 804) {
+      return "☁️"; // Clouds (with different intensity)
+    } else {
+      return "☀️"; // Clear sky (default)
+    }
+  }
+
+  String getMessage(double temp) {
+    if (temp > 25) {
+      return "It's 🍦 time!";
+    } else if (temp > 20) {
+      return "Time for shorts and 👕!";
+    } else if (temp < 10) {
+      return "You'll need 🧣 and 🧤!";
+    } else {
+      return "Bring a 🧥 just in case!";
+    }
+  }
+}
